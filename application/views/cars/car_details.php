462b9cb66d792365883a769ab5ce7adc884fcf68
khadim Raath <khadim.raath@incubasys.com>
2015-10-05 23:55:03 +0500

    car details added

--- application/views/cars/car_details.php
+++ application/views/cars/car_details.php
@@ -1,157 +1,135 @@
 <?php $this->load->view('include/header'); ?>
 <div id="page-wrap">
-  <div class="wrapper">
-    <div id="content">
-      <div class="heading-main similar-deals">NISSAN ELGRAND 3.5 PETROL AUTOMATIC V6</div>
-      <div id="single-slider">
-        <ul class="bxslider">
-          <li><img src="<?=  IMAGE_URL; ?>slide-image-one.jpg"  alt="Slide" /></li>
-          <li><img src="<?=  IMAGE_URL; ?>slide-image-two.jpg"  alt="Slide" /></li>
-          <li><img src="<?=  IMAGE_URL; ?>slide-image-one.jpg"  alt="Slide" /></li>
-          <li><img src="<?=  IMAGE_URL; ?>slide-image-two.jpg"  alt="Slide" /></li>
-          <li><img src="<?=  IMAGE_URL; ?>slide-image-one.jpg"  alt="Slide" /></li>
-          <li><img src="<?=  IMAGE_URL; ?>slide-image-two.jpg"  alt="Slide" /></li>
-        </ul>
-        <div id="bx-pager"> <a data-slide-index="0" href=""><img src="<?=  IMAGE_URL; ?>slide-image-one.jpg"  alt="Slide" /></a> <a data-slide-index="1" href=""><img src="<?=  IMAGE_URL; ?>slide-image-two.jpg"  alt="Slide" /></a> <a data-slide-index="2" href=""><img src="<?=  IMAGE_URL; ?>slide-image-one.jpg"  alt="Slide" /></a> <a data-slide-index="3" href=""><img src="<?=  IMAGE_URL; ?>slide-image-two.jpg"  alt="Slide" /></a> <a data-slide-index="4" href=""><img src="<?=  IMAGE_URL; ?>slide-image-one.jpg"  alt="Slide" /></a> <a data-slide-index="5" href=""><img src="<?=  IMAGE_URL; ?>slide-image-two.jpg"  alt="Slide" /></a> </div>
-      </div>
-      <!--Single Slider-->
-      <div id="VDDetailsPanel">
-        <h5>General - Toyota&nbsp;Prius+ | Info</h5>
-        <div class="detailbox">
-        <ul class="">
-          <li><strong>£19,990</strong><span>New Price:</span></li>
-          <li class="change-clor-list"><strong>£19,490</strong><span>Our Price:</span></li>
-          <li><strong>Estate</strong><span>Body Type:</span></li>
-          <li><strong>Automatic</strong><span>Trans:</span></li>
-          <li><strong>37,500</strong><span>Mileage:</span></li>
-          <li><strong>Hybrid</strong><span>Fuel Type:</span></li>
-          <li><strong>2013</strong><span>Reg Year:</span></li>
-          <li><strong>5</strong><span>Doors:</span></li>
-          <li><strong>1.8</strong><span>Engine Size:</span></li>
-          <li class="vdcolour"><strong>METALLIC TURQUOISE</strong><span>Colour:</span></li>
-          <div class="clear"></div>
-        </ul>
+    <div class="wrapper">
+        <div id="content">
+            <div class="heading-main similar-deals"><?= $car['make_title'] ?> , <?= $car['engine_size'] ?> , <?= $car['fuel'] ?> , <?= $car['transmission'] ?></div>
+            <div id="single-slider">
+                <ul class="bxslider">
+
+                    <li><img src="<?= $car['image_url']; ?>"  alt="Slide" /></li>
+                    <?php
+                    if ($images && !empty($images)) {
+                        foreach ($images as $key => $value) {
+                            ?>
+                            <li><img src="<?= base_url() . $value['image_url']; ?>"  alt="Slide" /></li>
+                            <?php
+                        }
+                    }
+                    ?>
+
+                </ul>
+                <div id="bx-pager">
+                    <a data-slide-index="0" href="">
+                        <img src="<?= $car['image_url']; ?>"  alt="Slide" />
+                    </a>
+                    <?php
+                    if ($images && !empty($images)) {
+                        foreach ($images as $key => $value) {
+                            ?>
+                            <a data-slide-index="<?= $key + 1 ?>" href="">
+                                <img src="<?= base_url() . $value['image_url']; ?>"  alt="Slide" />
+                            </a>
+                            <?php
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+            <!--Single Slider-->
+            <div id="VDDetailsPanel">
+                <h5>General - Info</h5>
+                <div class="detailbox">
+                    <ul class="">
+                        <li><strong>£<?= $car['price']; ?></strong><span>New Price:</span></li>
+                        <li class="change-clor-list"><strong>£<?= $car['price']; ?></strong><span>Our Price:</span></li>
+                        <li><strong>---</strong><span>Body Type:</span></li>
+                        <li><strong><?= $car['transmission']; ?></strong><span>Trans:</span></li>
+                        <li><strong><?= $car['mileage']; ?></strong><span>Mileage:</span></li>
+                        <li><strong><?= $car['fuel']; ?></strong><span>Fuel Type:</span></li>
+                        <li><strong><?= $car['year']; ?></strong><span>Reg Year:</span></li>
+                        <li><strong><?= $car['doors']; ?></strong><span>Doors:</span></li>
+                        <li><strong><?= $car['engine_size']; ?></strong><span>Engine Size:</span></li>
+                        <li class="vdcolour"><strong><?= $car['color']; ?></strong><span>Colour:</span></li>
+                        <div class="clear"></div>
+                    </ul>
+                </div>
+                <h5>Features</h5>
+                <div class="featuresbox">
+                    <ul class="">
+                        <?php
+                        if ($feature && !empty($feature)) {
+                            $features_arr = explode(",", $feature->list);
+                            if (!empty($features_arr)) {
+                                foreach ($features_arr as $feat) {
+                                    ?>
+                                    <li><?= $feat; ?></li>
+                                    <?php
+                                }
+                            }
+                        }
+                        ?>
+                        <div class="clear"></div>
+                    </ul>
+                </div>
+            </div>
+        </div><!--Content-->
+        <div id="sidebar">
+            <div class="widget">
+                <div class="prodect-detail-table">
+                    <table>
+                        <tr>
+                            <td>Miles</td>
+                            <td><?= $car['mileage']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Price</td>
+                            <td>£<?= $car['price']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Vehicle Type</td>
+                            <td>Wagons</td>
+                        </tr>
+                        <tr>
+                            <td>Transmission</td>
+                            <td><?= $car['transmission']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Exterior:</td>
+                            <td>ORANGE METALIC </td>
+                        </tr>
+                        <tr>
+                            <td>Interior</td>
+                            <td>BLACK LEATHER</td>
+                        </tr>
+                        <tr>
+                            <td>Engine Size</td>
+                            <td><?= $car['engine_size']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Fuel Type</td>
+                            <td><?= $car['fuel']; ?></td>
+                        </tr>
+                    </table>
+                </div>
+                <!--Product Detail Table-->
+                <h5>Contact To Buy :</h5>
+                <form id="register" action="<?= base_url(); ?>car/contact_to_buy" method="post">
+                    <input type="hidden" name="car_id" value="<?= $car['id']; ?>" />
+                    <input type="text" name="name" placeholder="Name" />
+                    <input type="email" name="email" placeholder="Email" />
+                    <textarea name="message" rows="3" placeholder="Message"></textarea>
+                    <input type="submit" value="Send"  />
+                </form>
+            </div>
+            <!--Widget--> 
         </div>
-        <h5>Toyota&nbsp;Prius+&nbsp;HYBRID EXCEL AUTO ESTATE 7SEAT BIMTA VVT-i FRESH IMPORT | Features</h5>
-        <div class="featuresbox">
-        <ul class="">
-            <li>7 Seats</li>
-            <li>DVD PLayer</li>
-            <li>Full BodyKit</li>
-            <li>Rear Parking Camera</li>
-            <li>UK Navigation / DVD (Optional)</li>
-            <li>Air Conditioning</li>
-            <li>Electric Mirrors</li>
-            <li>Fresh Import</li>
-            <li>Front FogLamps</li>
-            <li>Low Emissions</li>
-            <li>Low Road TAX</li>
-            <li>New MOT</li>
-            <li>Rear Centre Arm Rest</li>
-            <li>Rear Spoiler With Brake Light</li>
-            <li>Remote Central Locking</li>
-            <li>SRS AirBag</li>
-            <li>Tinted Glass</li>
-            <li>Anti-Lock Brakes</li>
-            <li>Drive and Run very well</li>
-            <li>Driver Arm Rest</li>
-            <li>Excellent Condition</li>
-            <li>Mileage Verified</li>
-            <li>Multiple Airbags</li>
-            <li>Part exchange welcome</li>
-            <li>Passenger Airbag</li>
-            <li>Power Assisted Steering</li>
-            <li>Radio/CD</li>
-            <li>Traction Control</li>
-            <li>Vehicle Stability Control</li>
-            <li>Driver's Airbag</li>
-            <li>Privacy Glass</li>
-            <li>Alloy Wheels</li>
-            <li>Central Locking</li>
-            <li>Electric Windows</li>
-            <li>Immobiliser</li>
-            <li>Split Folding Rear Seats</li>
-            <div class="clear"></div>
-        </ul>
-        </div>
-        <h5>Other Info</h5>
-        <div id="VDOtherinfo">
-        <ul>
-            <li>AVAILABLE FROM 25th April</li>
-            <li>FOR PCO-CAB-HIRE &amp; CHAUFFEURIING</li>
-            <li>WE OFFER BIMTA CHECK</li>
-            <li>WARRANTED LOW MILEAGE</li>
-            <li>CRUISE CONTROL</li>
-            <li>5TH GENERATION NEW SHAPE</li>
-            <li>CO2 emissions (g/km): 89*</li>
-            <li>FUEL ECONOMY (MPG): 72.4*</li>
-            <li>POWER HID LAMPS</li>
-            <li>FRESH IMPORT</li>
-            <li>FULLY SERVICED</li>
-            <li>EXCELLENT CONDITION</li>
-            <li>NEW MOT</li>
-            <li>FOR MORE STOCK &amp; DETAILS VISIT:</li>
-            <li>WWW.AUTOBASELTD.COM </li>
-        </ul>
-       </div>
-      </div>
-    </div><!--Content-->
-    <div id="sidebar">
-      <div class="widget">
-        <div class="prodect-detail-table">
-          <table>
-            <tr>
-              <td>Miles</td>
-              <td>36,000</td>
-            </tr>
-            <tr>
-              <td>Price</td>
-              <td>£6495</td>
-            </tr>
-            <tr>
-              <td>Vehicle Type</td>
-              <td>Wagons</td>
-            </tr>
-            <tr>
-              <td>Transmission</td>
-              <td>Automatic</td>
-            </tr>
-            <tr>
-              <td>Exterior:</td>
-              <td>ORANGE METALIC </td>
-            </tr>
-            <tr>
-              <td>Interior</td>
-              <td>BLACK LEATHER</td>
-            </tr>
-            <tr>
-              <td>Engine Size</td>
-              <td>2.0 PETROL</td>
-            </tr>
-            <tr>
-              <td>Transmission</td>
-              <td>Automatic</td>
-            </tr>
-          </table>
-        </div>
-        <!--Product Detail Table-->
-        <h5>Request More info :</h5>
-        <form>
-          <input type="text" name="" placeholder="Name" />
-          <input type="email" name="" placeholder="Email" />
-          <textarea rows="5" placeholder="Message"></textarea>
-          <input type="submit" value="Send"  />
-        </form>
-      </div>
-      <!--Widget--> 
+        <!--Side Bar-->
+        <div class="clear"></div>
     </div>
-    <!--Side Bar-->
-    <div class="clear"></div>
-  </div>
-  <!--wrapper--> 
+    <!--wrapper--> 
 </div>
 <!--Page Wrap-->
 <div id="product-container" class="home-product-container">
-	<?php $this->load->view("cars/available_cars_partial",array("heading"=>"Available Cars")); ?>
+    <?php $this->load->view("cars/available_cars_partial", array("heading" => "Available Cars")); ?>
 </div><!--Product Container-->
 <?php $this->load->view('include/footer'); ?>
